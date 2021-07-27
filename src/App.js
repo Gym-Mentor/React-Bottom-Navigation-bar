b45e884d4a86089568f5ae757307fdd027900ae4
박종한 <jonghan1983@naver.com>
2021-07-27 23:13:29 +0900

    Create Nav bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import First from "./pages/First";
+import Second from "./pages/Second";
+import Third from "./pages/Third";
+import Fourth from "./pages/Fourth";
+import Fifth from "./pages/Fifth";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/first" component={First} />
+        <Route path="/second" component={Second} />
+        <Route path="/third" component={Third} />
+        <Route path="/fourth" component={Fourth} />
+        <Route path="/fifth" component={Fifth} />
+      </Switch>
     </div>
   );
 }
